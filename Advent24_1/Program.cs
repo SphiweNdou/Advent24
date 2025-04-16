0f8e42c74cec335e5f26673329d16e8d820ef0cf
Sphiwe Ndou <ndebondou@gmail.com>
2025-04-16 18:46:34 +0200

    Added similarity score function for part 2 of question 1

--- Advent24_1/Program.cs
+++ Advent24_1/Program.cs
@@ -13,32 +13,62 @@ try
 {
     List<int> locationGroupA = new List<int>();
     List<int> locationGroupB = new List<int>();
-
     int count = 0;
+
     foreach (var line in File.ReadAllLines(inputFilePath))
     {
         var locationInputs = line.Split("  ");
+
         if ((locationInputs.Length > 0))
         {
             locationGroupA.Add(int.Parse(locationInputs[0]));
             locationGroupB.Add(int.Parse(locationInputs[1]));
         }
+
         count++;
     }
 
     locationGroupA.Sort();
     locationGroupB.Sort();
+    int totalDistance = TotalDistanceBetweenLocations(locationGroupA, locationGroupB);
+    int similarityScore = SimilarityScoreBetweenLocations(locationGroupA, locationGroupB);
 
+    Console.WriteLine($"The toatal Distance is: {totalDistance}");
+    Console.WriteLine($"The similarity score is: {similarityScore}");
+
+}
+catch ( Exception e)
+{
+    Console.WriteLine("Error:" + e.ToString());
+    Console.ReadLine();
+}
+
+static int TotalDistanceBetweenLocations(List<int> locationGroupA, List<int> locationGroupB)
+{
     int totalDistance = 0;
 
     for (int i = 0; i < locationGroupA.Count; i++)
     {
         totalDistance += Math.Abs(locationGroupA[i] - locationGroupB[i]);
     }
-    Console.WriteLine($"The toatal Distance is: {totalDistance}");
 
-} catch ( Exception e)
+    return totalDistance;
+}
+
+static int SimilarityScoreBetweenLocations(List<int> locationGroupA, List<int> locationGroupB)
 {
-    Console.WriteLine("Error:" + e.ToString());
-    Console.ReadLine();
+    int totalScore = 0;
+    List<int> uniqueLocation = new List<int>();
+
+    for (int i = 0; i < locationGroupA.Count; i++)
+    {
+        if (!uniqueLocation.Contains(locationGroupA[i]))
+        {
+            uniqueLocation.Add(locationGroupA[i]);
+            int matchesInGroupB = locationGroupB.Where(location => location == locationGroupA[i]).Count();
+            totalScore += matchesInGroupB * locationGroupA[i];
+        }
+    }
+
+    return totalScore;
 }
